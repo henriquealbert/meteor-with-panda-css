612a52bc30c7615d97cb17f28a00fabc145ab969
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-06-21 19:52:32 -0300

    remove comments

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Hello } from "./Hello.jsx";
 import { Info } from "./Info.jsx";
-// import { css } from "../../styled-system/css";
+import { css } from "../../styled-system/css";
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    {/* <div class={css({ fontSize: "2xl", fontWeight: "bold" })}>Hello 🐼!</div> */}
+    <div class={css({ fontSize: "2xl", fontWeight: "bold" })}>Hello 🐼!</div>
     <Hello />
     <Info />
   </div>
